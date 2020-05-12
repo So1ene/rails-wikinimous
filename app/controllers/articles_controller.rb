b669537916bd543ca75a8b468cce563e2efe5949
Solene Duprat <solene.duprat@gmail.com>
2020-05-12 16:24:49 -0400

    File structure and contents

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    new_article = Article.new(article_params)
+    new_article.save
+
+    redirect_to article_path(new_article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name, :address, :rating)
+  end
+end
